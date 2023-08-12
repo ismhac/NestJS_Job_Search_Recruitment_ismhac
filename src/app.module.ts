2e58163bf3c2fa94e6a405f38a775fd50a837ea1
ismhac <hachac2k2@gmail.com>
2023-08-12 16:44:33 +0700

    # Using MongoDB with NestJs

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
-  imports: [],
+  imports: [MongooseModule.forRoot('mongodb+srv://captainhac:12345@cluster0.7bskhhv.mongodb.net/')],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule { }
