eb0fc1a7d280e5f2c9d6aba4d0cbb18cee8b9e3a
ismhac <hachac2k2@gmail.com>
2023-09-16 16:54:39 +0700

    # using JWT

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,9 +1,13 @@
 import { Injectable } from '@nestjs/common';
 import { UsersService } from 'src/users/users.service';
+import { JwtService } from '@nestjs/jwt';
 
 @Injectable()
 export class AuthService {
-    constructor(private usersService: UsersService) { }
+    constructor(
+        private usersService: UsersService,
+        private jwtService: JwtService
+    ) { }
 
     // username/ pass là 2 tham số thư viện passport ném về
     async validateUser(username: string, pass: string): Promise<any> {
@@ -14,7 +18,16 @@ export class AuthService {
                 return user;
             }
         }
-
         return null;
+    }
+
+    async login(user: any) {
+        const payload = {
+            username: user.email,
+            sub: user._id
+        };
+        return {
+            access_token: this.jwtService.sign(payload),
+        };
     }
 }
