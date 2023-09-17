5651db22aa88d793dab9e1a43d784a362edfa937
ismhac <hachac2k2@gmail.com>
2023-09-17 12:48:47 +0700

    # soft-delete plugin

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,14 +5,18 @@ import { MongooseModule } from '@nestjs/mongoose';
 import { ConfigModule, ConfigService } from '@nestjs/config';
 import { UsersModule } from './users/users.module';
 import { AuthModule } from './auth/auth.module';
+import { softDeletePlugin } from 'soft-delete-plugin-mongoose';
 
 @Module({
   imports: [
-    // MongooseModule.forRoot('mongodb+srv://captainhac:12345@cluster0.7bskhhv.mongodb.net/'),
     MongooseModule.forRootAsync({
       imports: [ConfigModule],
       useFactory: async (configService: ConfigService) => ({
         uri: configService.get<string>('MONGO_URL'),
+        connectionFactory: (connection) => {
+          connection.plugin(softDeletePlugin);
+          return connection;
+        }
       }),
       inject: [ConfigService],
     }),
