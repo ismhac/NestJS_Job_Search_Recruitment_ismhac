cb206769f9f55727e3eba191ec56bae6fd95a169
ismhac <hachac2k2@gmail.com>
2023-09-23 00:21:37 +0700

    # Update User Schema

--- src/users/dto/create-user.dto.ts
+++ src/users/dto/create-user.dto.ts
@@ -1,15 +1,73 @@
-import { IsEmail, IsNotEmpty } from "class-validator";
+import { Type } from "class-transformer";
+import { IsEmail, IsNotEmpty, IsNotEmptyObject, IsObject, ValidateNested } from "class-validator";
+import mongoose from "mongoose";
 
 // data transfer object
+
+class Company {
+
+    @IsNotEmpty()
+    _id: mongoose.Schema.Types.ObjectId;
+
+    @IsNotEmpty()
+    name: string;
+}
+
 export class CreateUserDto {
-    @IsEmail()
-    @IsNotEmpty()
-    email: string;
 
-    @IsNotEmpty()
-    password: string;
-
+    @IsNotEmpty({ message: 'Name is required' })
     name: string;
 
+    @IsEmail()
+    @IsNotEmpty({ message: 'Email is required' })
+    email: string;
+
+    @IsNotEmpty({ message: 'Password is required' })
+    password: string;
+
+    @IsNotEmpty({ message: 'Age is required' })
+    age: number;
+
+    @IsNotEmpty({ message: 'Gender is required' })
+    gender: string;
+
+    @IsNotEmpty({ message: 'Address is required' })
     address: string;
+
+    @IsNotEmptyObject()
+    @IsObject()
+    @ValidateNested()
+    @Type(() => Company)
+    company: Company;
+}
+
+export class RegisterUserDto {
+
+    @IsNotEmpty({ message: 'Name is required' })
+    name: string;
+
+    @IsEmail()
+    @IsNotEmpty({ message: 'Email is required' })
+    email: string;
+
+    @IsNotEmpty({ message: 'Password is required' })
+    password: string;
+
+    @IsNotEmpty({ message: 'Age is required' })
+    age: number;
+
+    @IsNotEmpty({ message: 'Gender is required' })
+    gender: string;
+
+    @IsNotEmpty({ message: 'Address is required' })
+    address: string;
+
+    @IsNotEmpty({ message: 'Company is required' })
+    role: string;
+
+    @IsNotEmptyObject()
+    @IsObject()
+    @ValidateNested()
+    @Type(() => Company)
+    company: Company;
 }
