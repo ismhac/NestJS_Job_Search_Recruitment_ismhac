afe385055a166af31cbc9ea6e03143cf72f2eb9c
hienlehitek <hienle@hitek.com.vn>
2023-11-05 16:27:40 +0700

    # fix upload file

--- src/files/files.controller.ts
+++ src/files/files.controller.ts
@@ -7,22 +7,80 @@ import { UpdateFileDto } from './dto/update-file.dto';
 import { FilesService } from './files.service';
 import axios from 'axios';
 import { Response } from 'express';
+import { ConfigService } from '@nestjs/config';
+import { google } from 'googleapis';
+import fs from 'fs';
 
 @ApiTags('Files')
 @Controller('files')
 export class FilesController {
   constructor(
-    private readonly filesService: FilesService
+    private readonly filesService: FilesService,
+    private configService: ConfigService,
   ) { }
 
-  // @Public()
+  @Public()
   @Post('upload')
   @UseFilters(new HttpExceptionFilter())
   @ResponseMessage('Upload file successfully')
   @UseInterceptors(FileInterceptor('fileUpload'))
-  uploadFile(@UploadedFile() file: Express.Multer.File) {
+  async uploadFile(@UploadedFile() file: Express.Multer.File) {
+    const CLIENT_ID = this.configService.get<string>('CLIENT_ID');
+    const CLIENT_SECRET = this.configService.get<string>('CLIENT_SECRET');
+    const REFRESH_TOKEN = this.configService.get<string>('REFRESH_TOKEN');
+    const REFRESH_URI = this.configService.get<string>('REFRESH_URI');
+    const FOLDER_NAME = this.configService.get<string>('FOLDER_NAME');
+
+    const oauth2Client = new google.auth.OAuth2(CLIENT_ID, CLIENT_SECRET, REFRESH_URI);
+    oauth2Client.setCredentials({ refresh_token: REFRESH_TOKEN });
+
+    const drive = google.drive({
+      version: 'v3',
+      auth: oauth2Client
+    });
+
+
+    const folders = await drive.files.list({
+      q: `mimeType='application/vnd.google-apps.folder' and name='${FOLDER_NAME}'`,
+    });
+    let folderId;
+
+    if (folders.data.files.length === 0) {
+      // Thư mục chưa tồn tại, tạo thư mục mới
+      const folder = await drive.files.create({
+        requestBody: {
+          name: FOLDER_NAME,
+          mimeType: 'application/vnd.google-apps.folder',
+        },
+      });
+      folderId = folder.data.id;
+    } else {
+      // Thư mục đã tồn tại, lấy id của nó
+      folderId = folders.data.files[0].id;
+    }
+
+    // Tải file lên thư mục vừa tạo hoặc đã tồn tại
+    const response = await drive.files.create({
+      requestBody: {
+        name: file.filename,
+        mimeType: file.mimetype,
+        parents: [folderId], // Sử dụng id của thư mục làm cha
+      },
+      media: {
+        mimeType: file.mimetype,
+        body: fs.createReadStream(file.path),
+      },
+    });
+
+    fs.unlinkSync(file.path);
+
+    // Lấy đường dẫn trên Google Drive bằng cách kết hợp id với URL cơ bản của Google Drive
+    const fileUrl = `https://drive.google.com/file/d/${response.data.id}/view`;
+
     return {
       fileName: file.filename,
+      fileId: response.data.id,
+      fileUrl: fileUrl,
     }
   }
 
@@ -40,7 +98,7 @@ export class FilesController {
 
       if (fileExtension) {
         res.setHeader('Content-Type', contentType);
-        res.setHeader('Content-Disposition', `attachment; filename="downloaded-file.${fileExtension}"`);
+        res.setHeader('Content-Disposition', `attachment; filename="${fileName}.${fileExtension}"`);
       } else {
         throw new Error('The file type cannot be identified !');
       }
